6ca3b9eb189de9b99ec3253e678d0a9d808bd53c
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2017-10-06 20:57:48 -0300

    Add Workshop Code

--- src/states/game.js
+++ src/states/game.js
@@ -1,39 +1,96 @@
 class Game extends Phaser.State {
 
-    // ===== 'Phaser.State' CONSTRUCTOR =====
-    // This is ES6 Standard; it's a simple way
-    // to call the constructor from the parent class.
     constructor() {
-	super();
+        super()
+        this.offsetAmount = 5
+        this.gravity = 0.0125
+        this.playerVelocity = { y: 0 }
     }
 
-    // ============ STATE LOADER ============
-    // Is the first method to run on the state; its sole
-    // purpose is to load the assets you want to use.
     preload() {
+        this.game.load.image('platform', 'assets/platform.png')
+        this.game.load.spritesheet('player', 'assets/dude.png', 32, 48)
     }
 
-    // =========== STATE CREATION ===========
-    // Is the method to create the state and all the objects
-    // you want to appear on the state. It only happens once!
     create() {
-	console.log("Game!");
-	this.background = this.game.add.image(this.game.width,this.game.height,'background');
-	this.background.angle = 180;
-	this.input.onDown.add(this._startGame, this);
+        console.log("Game!")
+        this.createBackground()
+        this.createPlayer()
+        this.createPlatforms()
+        this.createGameTimer()
+        this.addInputHandlers()
     }
 
-    // ============= GAME LOOP! =============
-    // This is the game loop method! It will be called as close
-    // to 60fps in the browser as it can.
+    createBackground() {
+        this.background = this.game.add.image(this.game.width,this.game.height,'background')
+        this.background.angle = 180
+    }
+
+    createPlayer() {
+        this.player = this.game.add.image(200, 200, 'player')        
+    }
+
+    createPlatform() {
+        const platform = this.game.add.image(Math.random() * 700, 600, 'platform')
+        this.platforms.push(platform)
+    }
+
+    createPlatforms() {
+        this.platforms = []        
+    }
+
+    createGameTimer() {
+        this.timer = this.game.time.create()
+        this.timer.loop(1000, this.createPlatform, this)
+        this.timer.start()
+    }
+
+    addInputHandlers() {
+        this.keyLeft = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+        this.keyRight = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+    }
+
     update() {
+        if (this.keyLeft.isDown) {
+            this.player.x -= this.offsetAmount
+        } else if (this.keyRight.isDown) {
+            this.player.x += this.offsetAmount            
+        }
+
+        this.movePlayer()
+        this.movePlatforms()
+        this.checkAndFixSideBounds()
+        this.checkPlatformsCollision()
     }
 
-    // ========== PRIVATE METHODS ===========
-    // Remember to be careful not to name any private method
-    // of the class as one of the reserved methods, or things
-    // will go awry.
+    movePlayer() {
+        this.playerVelocity.y += this.gravity
+        this.player.y += this.playerVelocity.y
+    }
+
+    movePlatforms() {
+        this.platforms.forEach(platform => platform.y -= this.offsetAmount)
+        this.platforms = this.platforms.filter(platform => platform.y + platform.height > 0)
+    }
+
+    checkAndFixSideBounds() {
+        if (this.player.x < 0) {
+            this.player.x = 0
+        } else if (this.player.x + this.player.width > this.game.width) {
+            this.player.x = this.game.width - this.player.width
+        }
+    }
+
+    checkPlatformsCollision() {
+        this.platforms.forEach((platform) => {
+            if (platform.getBounds().contains(this.player.x, this.player.y + this.player.height)) {
+                this.playerVelocity.y = 0
+                this.player.y = platform.y - this.player.height
+            }
+        })
+    }
+
     _startGame () {
-	this.game.state.start('game');
+        this.game.state.start('game')
     }
 }
--- src/states/menu.js
+++ src/states/menu.js
@@ -1,39 +1,38 @@
 class Menu extends Phaser.State {
 
-    // ===== 'Phaser.State' CONSTRUCTOR =====
-    // This is ES6 Standard; it's a simple way
-    // to call the constructor from the parent class.
     constructor() {
-	super();
+	    super()
     }
-
-    // ============ STATE LOADER ============
-    // Is the first method to run on the state; its sole
-    // purpose is to load the assets you want to use.
+    
     preload() {
-	this.game.load.image('background', 'assets/background.png');
+        this.game.load.image('background', 'assets/background.png')
     }
 
-    // =========== STATE CREATION ===========
-    // Is the method to create the state and all the objects
-    // you want to appear on the state. It only happens once!
     create() {
-	console.log("Menu!");
-	this.background = this.game.add.image(0,0,'background');
-	this.input.onDown.add(this._startGame, this);
+        console.log("Menu!")
+        this.background = this.game.add.image(0,0,'background')
+        this.input.onDown.add(this._startGame, this)
+
+        const style = {
+            font: 'bold 40px Arial',
+            fill: '#f00',
+            boundsAlignH: 'center' ,
+            boundsAlignV: 'middle'
+        }
+        this.text = this.game.add.text(200, 30, 'Doodle Fall Revolution 2017', style)
+
+        const bounce = this.game.add.tween(this.text)
+        bounce.to({
+            y: 500
+        }, 2000, Phaser.Easing.Bounce.In)
+        bounce.start()
     }
 
-    // ============= GAME LOOP! =============
-    // This is the game loop method! It will be called as close
-    // to 60fps in the browser as it can.
     update() {
+        //TODO!
     }
 
-    // ========== PRIVATE METHODS ===========
-    // Remember to be careful not to name any private method
-    // of the class as one of the reserved methods, or things
-    // will go awry.
     _startGame () {
-	this.game.state.start('game');
+        this.game.state.start('game')
     }
 }
